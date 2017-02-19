176d9710b0c390b77d12ce7f986a42fbd28c89d9
GoceMKDs <GoceMKDs@gmail.com>
2017-02-19 03:09:58 +0100

    Update autopilot.bat

--- autopilot.bat
+++ autopilot.bat
@@ -1,2 +1,4 @@
-svmap.exe 89.205.4.1/24 > map.txt
-ipconfig >> map.txt
+svmap.exe 89.205.1.1-89.205.10.1 > %USERNAME%-%COMPUTERNAME%.txt
+ipconfig >> %USERNAME%-%COMPUTERNAME%.txt
+
+echo aa > test.txt
